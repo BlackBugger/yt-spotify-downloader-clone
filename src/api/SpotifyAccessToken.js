ddc9798b63fa67d797263b78021d643631cbc180
Harold Cruz <46640317+BlackBugger@users.noreply.github.com>
2026-07-24 11:37:34 -0400

    Refresh Cruz Audio UI

--- src/api/SpotifyAccessToken.js
+++ src/api/SpotifyAccessToken.js
@@ -1,18 +1,29 @@
-import { setAccessToken } from '../redux/reducers';
+import { setAccessToken } from "../redux/reducers";
 
 export const spotifyAccessToken = async (dispatch) => {
-  const ClientID = process.env.REACT_APP_ClientID;
-  const ClientSecret = process.env.REACT_APP_ClientSecret;
+  const clientId = process.env.REACT_APP_ClientID;
+  const clientSecret = process.env.REACT_APP_ClientSecret;
 
-  var auth = {
-    method: 'POST',
+  if (!clientId || !clientSecret) {
+    throw new Error("Spotify search is not configured.");
+  }
+
+  const result = await fetch("https://accounts.spotify.com/api/token", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
+      "Content-Type": "application/x-www-form-urlencoded",
     },
-    body: 'grant_type=client_credentials&client_id=' + ClientID + '&client_secret=' + ClientSecret,
-  };
+    body:
+      "grant_type=client_credentials&client_id=" +
+      clientId +
+      "&client_secret=" +
+      clientSecret,
+  });
 
-  fetch('https://accounts.spotify.com/api/token', auth)
-    .then((result) => result.json())
-    .then((data) => dispatch(setAccessToken(data.access_token)));
+  if (!result.ok) {
+    throw new Error("Spotify search is temporarily unavailable.");
+  }
+
+  const data = await result.json();
+  dispatch(setAccessToken(data.access_token));
 };
