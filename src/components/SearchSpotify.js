e03e65205d5b4361b3cd60288d2df07102ea9d3d
Harold Cruz <46640317+BlackBugger@users.noreply.github.com>
2026-07-24 11:37:32 -0400

    Refresh Cruz Audio UI

--- src/components/SearchSpotify.js
+++ src/components/SearchSpotify.js
@@ -1,33 +1,33 @@
-
-import { setTrack } from '../redux/reducers';
+import { setTrack } from "../redux/reducers";
 import { store } from "../redux/store";
 
-export const searchSpotify = async (dispatch)=> {
+export const searchSpotify = async (dispatch, query) => {
+  const accessToken = store.getState().accessToken.accessToken;
+  const searchInput = query || store.getState().searchInput.searchInput;
 
-  const accessToken = store.getState().accessToken.accessToken
-  const searchInput = store.getState().accessToken.searchInput
+  if (!accessToken) {
+    throw new Error("Search is still getting ready. Please try again in a moment.");
+  }
 
-  
-  // Search
-  var searchParam = {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      Authorization: 'Bearer ' + accessToken,
-    },
-  };
+  const response = await fetch(
+    "https://api.spotify.com/v1/search?q=" +
+      encodeURIComponent(searchInput.trim()) +
+      "&type=track&limit=12",
+    {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + accessToken,
+      },
+    }
+  );
 
-  // Track
-  await fetch(
-    'https://api.spotify.com/v1/search?q=' +
-      searchInput +
-      '&type=track&limit=20',
-    searchParam
-  )
-    .then((response) => response.json())
-    .then((data) => {
-      dispatch(setTrack(data.tracks.items));
-    });
-}
+  if (!response.ok) {
+    throw new Error("We couldn't complete that search. Please try again.");
+  }
 
-
+  const data = await response.json();
+  const tracks = data?.tracks?.items || [];
+  dispatch(setTrack(tracks));
+  return tracks;
+};
