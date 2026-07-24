3997cddb06ee35b8d823300e4fbb965f2942101c
Harold Cruz <46640317+BlackBugger@users.noreply.github.com>
2026-07-24 11:37:20 -0400

    Refresh Cruz Audio UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,20 @@
-/* eslint-disable */
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Dashboard from "./pages/Dashboard";
-import Home from "./pages/Home"
-import { setCode } from './redux/reducers';
-import React from 'react';
-import { Routes, Route } from 'react-router-dom'
-const codec = new URLSearchParams(window.location.search).get('code')
+import Home from "./pages/Home";
+import { setCode } from "./redux/reducers";
 
 function App() {
   const dispatch = useDispatch();
-  dispatch(setCode(codec));
+  const { code } = useSelector((state) => state.code);
 
-  const { accessToken } = useSelector((state) => state.accessToken)
-  console.log(accessToken);
-  const { code } = useSelector((state) => state.code)
+  useEffect(() => {
+    const authorizationCode = new URLSearchParams(window.location.search).get("code");
+    dispatch(setCode(authorizationCode || ""));
+  }, [dispatch]);
 
-  console.log(code);
-  return (
-
-    // <Routes>
-    //   <Route path="/" exact element={<Home />} />
-    //   <Route path="/home" exact element={<Dashboard code={code} />} />
-
-    // </Routes>
- code ? <Dashboard code={code}/> : <Home/>
-  )
-
-
-
-  
+  return code ? <Dashboard code={code} /> : <Home />;
 }
 
 export default App;
